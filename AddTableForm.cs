14ce621ef3be3ceb29dd88511b5bbc4dc23fc7c5
cj123462 <cj123462@gmail.com>
2016-12-11 21:28:32 +0200

    Add files via upload

---
+++ AddTableForm.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Admin
+ * Date: 11.12.2016
+ * Time: 21:13
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Zverev_Kursova_OBD
+{
+	/// <summary>
+	/// Description of AddTableForm.
+	/// </summary>
+	public partial class AddTableForm : Form
+	{
+		public AddTableForm()
+		{
+			InitializeComponent();
+		}
+
+		
+		void AddTableFormLoad(object sender, EventArgs e)
+		{
+			MySQL mysql = new MySQL();
+			mysql.connect("localhost","root","1","zverev_kursova_obd");
+		}
+		
+		void AddTableButtonClick(object sender, EventArgs e)
+		{
+			if(NewTableTextBox.Text!=""){
+				MySQL mysql = new MySQL();
+				mysql.exWithoutResult(@"create table "+NewTableTextBox.Text+
+				                     " like холодильники;");
+			} else MessageBox.Show("Введіть назву таблиці","Error");
+		}
+	}
+}
